5738f715166151bf8f2ac2625c4d3c8eee43cada
ChunqiangFan <515235987@qq.com>
2019-06-05 17:54:29 +0800

    1.增加java-scala调用实例 2.增加spark2.3-scala2.11.12版本Wordcount实例，支持yarn client和yarn cluster模式

---
+++ scala-java-demo/src/main/java/com/cdh/ja/App.java
@@ -0,0 +1,11 @@
+package com.cdh.ja;
+
+import com.cdh.sc.ScalaMethod;
+
+public class App {
+    public static void main(String[] a){
+        System.out.println("Hello Java");
+        ScalaMethod sm = new ScalaMethod();
+        sm.main(a);
+    }
+}
---
+++ scala-java-demo/src/main/scala/com/cdh/sc/ScalaMethod.scala
@@ -0,0 +1,7 @@
+package com.cdh.sc
+
+class ScalaMethod {
+  def main(args: Array[String]) {
+    println("Hello Scala Method")
+  }
+}
